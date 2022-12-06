130060465dd0fd5b390213c755bf07658ac2169b
Kevin Koobs <k.koobs@outlook.com>
2022-12-06 21:12:54 +0100

    Setup tailwind and first tests

---
+++ app/components/Option.tsx
@@ -0,0 +1,7 @@
+export default function Option(props: any) {
+    return (
+        <div className="w-full h-[20vh] p-4">
+            <h3 className="text-xl font-bold">Test 123</h3>
+        </div>
+    )
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,14 @@
+import Option from "~/components/Option";
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="font-sans relative w-screen h-screen bg-[#f3f3f3]">
+      <div className="w-[250px] bg-white h-screen border-r-4">
+        <Option />
+        <Option />
+        <Option />
+        <Option />
+      </div>
     </div>
   );
 }
